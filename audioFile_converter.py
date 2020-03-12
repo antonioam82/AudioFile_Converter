d8b853a18324d06583560a990512851981edc7fb
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-03-12 19:32:09 +0100

    Create audioFile_converter.py

---
+++ audioFile_converter.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import tkinter
+import pydub
+
+root = tkinter.Tk()
+root.title("AUDIO FILE CONVERTER")
+root.configure(background="gray40")
+root.geometry("700x500")
+
+
+
+root.mainloop()

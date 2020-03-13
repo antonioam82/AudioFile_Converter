3a8688d3ccc402ca64cffdd71f1ba8673312686b
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-03-13 16:49:01 +0100

    Update audioFile_converter.py

--- audioFile_converter.py
+++ audioFile_converter.py
@@ -11,6 +11,12 @@ etiName = Label(root,text='NINGÚN ARCHIVO SELECCIONADO',bg="black",
                 fg="red",width=91,height=2)
 etiName.place(x=26,y=80)
 
+btnBusca = Button(root,text='BUSCAR ARCHIVO',bg='blue',fg='firebrick1')
+btnBusca.place(x=291,y=150)
+
+btnWav = Button(root,text='CONVERTIR A .WAV',bg='red',fg='white',width=40)
+btnWav.place(x=26,y=230)
+
 
 
 root.mainloop()

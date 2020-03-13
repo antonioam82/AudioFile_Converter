b9f260ccf2ea8e8a523cb32e2fdbf36d15ace046
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-03-13 17:14:01 +0100

    Update audioFile_converter.py

--- audioFile_converter.py
+++ audioFile_converter.py
@@ -6,25 +6,26 @@ root = tkinter.Tk()
 root.title("AUDIO FILE CONVERTER")
 root.configure(background="gray40")
 root.geometry("700x500")
+actf = 'red'
 
 etiName = Label(root,text='NINGÚN ARCHIVO SELECCIONADO',bg="black",
                 fg="red",width=91,height=2)
 etiName.place(x=26,y=80)
 
-btnBusca = Button(root,text='BUSCAR ARCHIVO',bg='blue',fg='firebrick1')
+btnBusca = Button(root,text='BUSCAR ARCHIVO',activebackground='firebrick1',activeforeground='blue',bg='blue',fg='firebrick1')
 btnBusca.place(x=291,y=150)
 
-btnWav = Button(root,text='CONVERTIR A .WAV',bg='red',fg='white',width=40)
+btnWav = Button(root,text='CONVERTIR A .WAV',activeforeground=actf,bg='red',fg='white',width=40)
 btnWav.place(x=26,y=230)
-btnMp3 = Button(root,text='CONVERTIR A .MP3',bg='red',fg='white',width=40)
+btnMp3 = Button(root,text='CONVERTIR A .MP3',activeforeground=actf,bg='red',fg='white',width=40)
 btnMp3.place(x=26,y=280)
-btnFlv = Button(root,text='CONVERTIR A .FLV',bg='red',fg='white',width=40)
+btnFlv = Button(root,text='CONVERTIR A .FLV',activeforeground=actf,bg='red',fg='white',width=40)
 btnFlv.place(x=26,y=330)
-btnOgg = Button(root,text='CONVERTIR A .OGG',bg='red',fg='white',width=40)
+btnOgg = Button(root,text='CONVERTIR A .OGG',activeforeground=actf,bg='red',fg='white',width=40)
 btnOgg.place(x=380,y=230)
-btnWma = Button(root,text='CONVERTIR A .WMA',bg='red',fg='white',width=40)
+btnWma = Button(root,text='CONVERTIR A .WMA',activeforeground=actf,bg='red',fg='white',width=40)
 btnWma.place(x=380,y=280)
-btnMp4 = Button(root,text='CONVERTIR A .MP4',bg='red',fg='white',width=40)
+btnMp4 = Button(root,text='CONVERTIR A .MP4',activeforeground=actf,bg='red',fg='white',width=40)
 btnMp4.place(x=380,y=330)
 
 

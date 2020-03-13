543170ae8f54072bc92de86fec8c6dbe7a00b843
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-03-13 16:19:44 +0100

    Update audioFile_converter.py

--- audioFile_converter.py
+++ audioFile_converter.py
@@ -7,6 +7,11 @@ root.title("AUDIO FILE CONVERTER")
 root.configure(background="gray40")
 root.geometry("700x500")
 
+etiName = Label(root,text='NINGÚN ARCHIVO SELECCIONADO',bg="black",
+                fg="red",width=91,height=2)
+etiName.place(x=26,y=80)
+
 
 
 root.mainloop()
+
